08d549c5ecb626facd1b91830f706977bf248e6b
KrushnaDike <dikekrishna155@gmail.com>
2024-07-20 17:49:19 +0530

    cors setup

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
